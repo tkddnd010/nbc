f6ec665601f982bb3c2d9c53bf9edc5173083d36
tkddnd010 <tkddnd010@naver.com>
2024-01-09 16:21:01 +0900

    데이터 받아오는 기능과 보여주는 기능 파일 분리

--- src/movieView.js
+++ src/movieView.js
@@ -1,3 +1,4 @@
+import { fetchMovieData } from "./fetchData.js";
 export const viewMovieCard = async () => {
     const movies = await fetchMovieData();
 
@@ -14,17 +15,4 @@ export const viewMovieCard = async () => {
         )
         .join("");
 
-    async function fetchMovieData() {
-        const options = {
-            method: 'GET',
-            headers: {
-                accept: 'application/json',
-                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OGQ0MTY5ZDNlZDMwMWMyMWU1MzFlNzlmZWE3YmU3NiIsInN1YiI6IjY1OTNjMjMwZTAwNGE2NmQ2MzE4M2IyYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dD4evSXC1tVDr9ykCcM2brykSnx2y5tnBayRMSsGgXw'
-            },
-        };
-
-        const response = await fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options);
-        const res = await response.json();
-        return res.results;
-    }
 }
