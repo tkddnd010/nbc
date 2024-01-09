e29b3891abd7f5005befa49254ad8fe0c3224b3d
tkddnd010 <tkddnd010@naver.com>
2024-01-09 19:50:13 +0900

    API_KEY숨김 코드

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,13 +1,18 @@
+import { config } from "../apikey.js";
 export const fetchMovieData = async () => {
-    const options = {
-        method: 'GET',
-        headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OGQ0MTY5ZDNlZDMwMWMyMWU1MzFlNzlmZWE3YmU3NiIsInN1YiI6IjY1OTNjMjMwZTAwNGE2NmQ2MzE4M2IyYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dD4evSXC1tVDr9ykCcM2brykSnx2y5tnBayRMSsGgXw'
-        },
-    };
+  const API_KEY = config.apikey;
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: API_KEY,
+    },
+  };
 
-    const response = await fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options);
-    const res = await response.json();
-    return res.results;
-}
+  const response = await fetch(
+    "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+    options
+  );
+  const res = await response.json();
+  return res.results;
+};
